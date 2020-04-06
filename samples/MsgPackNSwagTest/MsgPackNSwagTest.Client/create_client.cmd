1b6d423748e5d52c8aacc8675cc1738aaa183d90
lolochristen <laurent@laurent-christen.ch>
2020-04-06 17:04:29 +0200

    Update to use OpenApiReference in csproj.

--- samples/MsgPackNSwagTest/MsgPackNSwagTest.Client/create_client.cmd
+++ samples/MsgPackNSwagTest/MsgPackNSwagTest.Client/create_client.cmd
@@ -1,4 +1,5 @@
 echo off
+rem Alternative way to create to classes from commandline instead of using OpenApiReference generator.
 echo messagepack
 call nswag openapi2csclient /input:https://localhost:5001/swagger/v1/swagger.json /classname:ContactsService  /namespace:MsgPackNSwagTest.Client.MessagePack  /output:.\MessagePack\ContactsService.cs /templatedirectory:..\..\..\src\MessagePack.NSwag\Templates
 
